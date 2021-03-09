c72f70311faf33765fd3f92981e95b80b3cb35fc
yasha6081 <yasha6081@gmail.con>
2021-03-09 10:39:13 -0600

    init

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('views', 'views');
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req, res) => {
-    res.send("Welcome to our new app")
+    res.render('home-guest')
 });
 
 
