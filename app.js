6eee523c3acc815d22bc0149013689616703b6ed
yasha6081 <yasha6081@gmail.con>
2021-03-09 14:57:28 -0600

    ejs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs')
-
 
 app.get('/', (req, res) => {
     res.render('home-guest')
