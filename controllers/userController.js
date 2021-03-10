7b8220fb8a46a87f4367024bb405649cfaaa6343
yasha6081 <yasha6081@gmail.con>
2021-03-09 20:57:59 -0600

    a

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.login = () => {
+
+};
+
+exports.logout = () => {
+      
+};
+
+exports.register = (req, res) => {
+    res.send("Thank for tryin to register")   
+};
+
+exports.home = (req, res) => {
+    res.render('home-guest')
+};
--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('./controllers/userController')
 
 
-router.get('/', (req, res) => {
-    res.render('home-guest')
-});
+router.get('/', userController.home);
+router.post('/register', userController.register)
+
 
 
 
