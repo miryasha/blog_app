9eced7d026b21aacf795cdd52db5faea7389e59e
yasha6081 <yasha6081@gmail.con>
2021-03-11 09:49:28 -0600

    user name has checked

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+const validator = require('validator');
+
 let User = function(data)  {
    this.data = data;
    this.errors = [];
@@ -7,7 +9,8 @@ let User = function(data)  {
 
 User.prototype.validate = function(){
    if (this.data.username == "") {this.errors.push("You must provide a username.")}
-   if (this.data.email == "") {this.errors.push("You must provide a vlid email.")}
+   if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username aca onl conarin letters and numbers.")}
+   if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a vlid email.")}
    if (this.data.password == "") {this.errors.push("You must provide a password.")}
    if(this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 caracters")}
    if(this.data.password.length > 100) {this.errors.push("Password can not exceed 100 characters.")}
