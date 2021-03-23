7eb08938e36e18abcfd740f9d1fbb832521b41e9
yasha6081 <yasha6081@gmail.con>
2021-03-23 12:07:45 -0500

    cookies

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 
 mongodb.connect(process.env.CONNECTIONSTRNG,{useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
-   module.exports = client.db()
+   module.exports = client
    const app = require('./app')
    app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
