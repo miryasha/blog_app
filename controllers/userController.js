1b902d7b386e947ff80fc05b02b8f0ae668e5a09
yasha6081 <yasha6081@gmail.con>
2021-03-23 10:33:48 -0500

    ejs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,28 @@
 const User = require('../models/User')
 
-exports.login = (req, res) => {
-    let user = new User(req.body)
-    user.login()
-    user.login().then(function(result) {
-        res.send(result)
-    }).catch((err) => {
-        res.send(err)
-    })
+exports.login = function(req, res) {
+  let user = new User(req.body)
+  user.login().then(function(result) {
+    res.send(result)
+  }).catch(function(e) {
+    res.send(e)
+  })
+}
 
+exports.logout = function() {
+  
+}
 
-};
+exports.register = function(req, res) {
+  let user = new User(req.body)
+  user.register()
+  if (user.errors.length) {
+    res.send(user.errors)
+  } else {
+    res.send("Congrats, there are no errors.")
+  }
+}
 
-exports.logout = () => {
-      
-};
-
-exports.register = (req, res) => {
-    let user = new User(req.body)
-    user.register();
-    if(user.errors.length) {
-       res.send(user.errors)
-    } else {
-       res.send("Congrats, ther arre no errors.")
-    };  
-};
-
-exports.home = (req, res) => {
-    res.render('home-guest')
-};
+exports.home = function(req, res) {
+  res.render('home-guest')
+}
